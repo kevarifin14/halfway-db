4dd489796fdd7a61d597868140ca38ea41ce31a1
Kevin Arifin <kevarifin14@gmail.com>
2015-08-26 22:49:29 -0700

    change date to datetime

--- db/migrate/20150827045528_change_date_format_for_events.rb
+++ db/migrate/20150827045528_change_date_format_for_events.rb
@@ -1,6 +1,5 @@
 class ChangeDateFormatForEvents < ActiveRecord::Migration
   def change
-    remove_column :events, :date, :date, null: false
-    add_column :events, :date, :datetime, null: false
+    change_column :events, :date, :datetime
   end
 end
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :event do
     description 'Event'
-    date '1996-01-03'.to_date
+    date '1996-01-03'.to_datetime
   end
 end
