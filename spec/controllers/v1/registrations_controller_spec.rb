a2d46b7a3619cf75d76f52bf6237958e85d9935b
Kevin Arifin <kevarifin14@gmail.com>
2015-12-26 19:52:46 -0800

    Add spec for registrations controller

---
+++ spec/controllers/v1/registrations_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe V1::RegistrationsController do
+  describe 'POST #create' do
+    def post_create
+      post :create,
+           email: email,
+           username: username,
+           password: password,
+           password_confirmation: password,
+           longitude: longitude,
+           latitude: latitude
+    end
+
+    let(:email) { 'user@foo.com' }
+    let(:username) { 'user' }
+    let(:password) { 'password' }
+    let(:longitude) { 69.0 }
+    let(:latitude) { 54.0 }
+    let(:user) { User.last }
+
+    it 'creates a new user' do
+      expect { post_create }.to change(User, :count).by(1)
+    end
+
+    it 'renders json for the user' do
+      post_create
+      expect(response.body).to eq(
+        "{\"email\":\"#{user.email}\",\"token_type\":\"Bearer\","\
+        "\"user_id\":#{user.id},"\
+        "\"access_token\":\"#{user.access_token}\","\
+        "\"username\":\"#{user.username}\"}"
+      )
+    end
+  end
+end
