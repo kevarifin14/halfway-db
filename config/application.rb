da1c077c4d47f545b1d3b0333a0e455b6a317cb3
Kevin Arifin <kevarifin14@gmail.com>
2016-01-02 15:33:28 -0800

    Precompile assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module HalfwayDb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true # precompile assets for heroku
     config.generators do |g|
       # g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
