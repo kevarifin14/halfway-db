257f56441ed791af698c688d3a0c9eb4074886be
Kevin Arifin <kevarifin14@gmail.com>
2016-01-11 19:32:17 -0800

    Halfway location is calculated based only on the users that are going

--- lib/halfway_calculator.rb
+++ lib/halfway_calculator.rb
@@ -3,8 +3,14 @@ require 'method_object'
 HalfwayCalculator = MethodObject.new(:event) do
   def call
     {
-      latitude: event.users.average(:latitude),
-      longitude: event.users.average(:longitude),
+      latitude: going_users.sum(&:latitude) / going_users.count,
+      longitude: going_users.sum(&:longitude) / going_users.count,
     }
+  end
+
+  private
+
+  def going_users
+    User.accepted_event_invitation(event)
   end
 end
--- spec/lib/halfway_calculator_spec.rb
+++ spec/lib/halfway_calculator_spec.rb
@@ -3,10 +3,35 @@ require 'halfway_calculator'
 
 RSpec.describe HalfwayCalculator do
   let(:event) { create(Event, event_params) }
-  let(:event_params) { attributes_for(Event).merge(users) }
-  let(:users) { { users: [first_user, second_user] } }
-  let(:first_user) { create(User, latitude: 15, longitude: 15) }
-  let(:second_user) { create(User, latitude: 45, longitude: 45) }
+  let(:event_params) { attributes_for(Event).merge(invitations) }
+  let(:invitation_1) { create(Invitation, rsvp: true, user: first_user) }
+  let(:invitation_2) { create(Invitation, rsvp: true, user: second_user) }
+  let(:invitation_3) { create(Invitation, rsvp: false, user: third_user) }
+  let(:users) { { users: [first_user, second_user, third_user] } }
+  let(:invitations) do
+    { invitations: [invitation_1, invitation_2, invitation_3] }
+  end
+  let(:first_user) do
+    create(
+      User,
+      latitude: 15,
+      longitude: 15,
+    )
+  end
+  let(:second_user) do
+    create(
+      User,
+      latitude: 45,
+      longitude: 45
+    )
+  end
+  let(:third_user) do
+    create(
+      User,
+      latitude: 13,
+      longitude: 32
+    )
+  end
 
   it 'calculates the correct halfway location' do
     expect(described_class.call(event: event)).to eq(
