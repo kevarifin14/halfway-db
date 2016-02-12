a67c6d3197634121ad5593fdc9bb863510471842
Kevin Arifin <kevarifin14@gmail.com>
2016-02-12 00:30:34 -0800

    Fixes to phone controller

--- app/controllers/V1/phone_numbers_controller.rb
+++ app/controllers/V1/phone_numbers_controller.rb
@@ -3,10 +3,12 @@ require './app/serializers/V1/sessions_serializer'
 module V1
   # Verification for phone numbers
   class PhoneNumbersController < ApplicationController
+    skip_before_action :authenticate_user_from_token!
+
     def verify
       @phone_number =
-        PhoneNumber.find_by(phone_number: params.fetch(:hidden_phone_number))
-      @phone_number.verify(params.fetch(:pin))
+        PhoneNumber.find_by(phone_number: params.require(:hidden_phone_number))
+      @phone_number.verify(params.require(:pin_number))
       render json: @user, serializer: V1::SessionsSerializer, root: nil
     end
   end
