5ccfa7f1d4fec3a08d0e2cfb02bb3a8fa27981ef
Kevin Arifin <kevarifin14@gmail.com>
2016-02-12 00:13:33 -0800

    Fix spelling mistake of pages controller

--- app/controllers/V1/phone_numbers_controller.rb
+++ app/controllers/V1/phone_numbers_controller.rb
@@ -2,7 +2,7 @@ require './app/serializers/V1/sessions_serializer'
 
 module V1
   # Verification for phone numbers
-  class PhoneNumbersConroller < ApplicationController
+  class PhoneNumbersController < ApplicationController
     def verify
       @phone_number =
         PhoneNumber.find_by(phone_number: params.fetch(:hidden_phone_number))
