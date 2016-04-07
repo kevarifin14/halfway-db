78871187ca4ce4488d9e5058b0375b43f8339797
Kevin Arifin <kevarifin14@gmail.com>
2016-04-07 01:31:09 -0700

    Minor fix to loop logic

--- app/controllers/V1/friends_controller.rb
+++ app/controllers/V1/friends_controller.rb
@@ -17,12 +17,13 @@ module V1
         if user.id != current_user.id
           contacts.each do |contact|
             phone_numbers = contact.fetch('phoneNumbers')
-            break if phone_numbers.nil?
-            phone_numbers.each do |phoneNumber|
-              puts phoneNumber.fetch('value').phony_normalized
-              if phoneNumber.fetch('value').phony_normalized == user.phone_number
-                friends_list << attributes(user, contact)
-                break
+            if not phone_numbers.nil?
+              phone_numbers.each do |phoneNumber|
+                puts phoneNumber.fetch('value').phony_normalized
+                if phoneNumber.fetch('value').phony_normalized == user.phone_number
+                  friends_list << attributes(user, contact)
+                  break
+                end
               end
             end
           end
